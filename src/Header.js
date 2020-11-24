42cd34c270ce5c254ad53b5739921aa6ffcd63f5
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-11-25 01:51:02 +0500

    adding search icons

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import "./Header.css";
+import SearchIcon from '@material-ui/icons/Search';
 
 
 function Header() {
     return  <div className="header">
          <div className="header__left">
              <img src="https://1000logos.net/wp-content/uploads/2016/11/Facebook-logo.png" alt='facebook logo'/>
+             <div className="header__input">
+                 <SearchIcon />
+                 <input type="text" />
+             </div>
          </div>  
             
          <div className="header__middle"></div> 
