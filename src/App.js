efb479f35369317d8cf97e38cd87c7cdfcbe54ef
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-11-25 01:17:02 +0500

    add firebase config

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>lets clone facebook</h1>
     </div>
   );
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBkBHdXuLV5zwofvwhcUgijE15__9oCvMw",
+    authDomain: "facebook-clone-81527.firebaseapp.com",
+    databaseURL: "https://facebook-clone-81527.firebaseio.com",
+    projectId: "facebook-clone-81527",
+    storageBucket: "facebook-clone-81527.appspot.com",
+    messagingSenderId: "700130552691",
+    appId: "1:700130552691:web:47c32d4e87e4c89bf2b306",
+    measurementId: "G-1DY5YXGPTV"
+  };
