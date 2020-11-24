0ffae34c7120413bc054d0a6f94b2462fad3beba
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-11-25 01:47:56 +0500

    install materail UI icons

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Header.css";
+
+
+function Header() {
+    return  <div className="header">
+         <div className="header__left">
+             <img src="https://1000logos.net/wp-content/uploads/2016/11/Facebook-logo.png" alt='facebook logo'/>
+         </div>  
+            
+         <div className="header__middle"></div> 
+
+         <div className="header__right"></div>  
+         
+
+        </div>;
+    
+}
+
+export default Header
