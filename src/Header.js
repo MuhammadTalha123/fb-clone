0c9e276b39eb349d8bd93f0fa14b3b68a981956b
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-11-25 02:05:19 +0500

    adding materail UI icons

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import "./Header.css";
 import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+
 
 
 function Header() {
@@ -13,7 +19,23 @@ function Header() {
              </div>
          </div>  
             
-         <div className="header__middle"></div> 
+         <div className="header__middle">
+             <div className="header__option">
+                <HomeIcon fontSize="large" />
+             </div>
+             <div className="header__option">
+                <FlagIcon fontSize="large" />
+             </div>
+             <div className="header__option">
+                <SubscriptionsIcon fontSize="large" />
+             </div>
+             <div className="header__option">
+                <StorefrontIcon fontSize="large" />
+             </div>
+             <div className="header__option">
+                <SupervisedUserCircleIcon fontSize="large" />
+             </div>
+         </div> 
 
          <div className="header__right"></div>  
          
