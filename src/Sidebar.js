c840635313b31bec0a30fc7f4dfa14bb1072fd36
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-11-26 00:10:18 +0500

    adding sidebar and sidebar row

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
+import "./Sidebar.css";
+import SidebarRow from "./SidebarRow";
 
 function Sidebar() {
-    return <div className="sidebar"></div>
+    return <div className="sidebar">
+        <SidebarRow />
+    </div>
 }
 
 export default Sidebar
---
+++ src/SidebarRow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./SidebarRow.css";
+
+const SidebarRow = (porps) => {
+    return (
+        <div className="sidebarRow">
+            <p>Row</p>
+        </div>
+    )
+}
+
+export default SidebarRow;
